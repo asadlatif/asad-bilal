a0ba6bd392dcc744ea96a30daf1a66d574d14272
bilal <bilalhusain1194@gmail.com>
2020-01-02 11:22:34 -0800

    email work

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,6 +83,16 @@ DATABASES = {
     }
 }
 
+DEFAULT_FROM_EMAIL = 'raconnectors@gmail.com'
+SERVER_EMAIL = 'raconnectors@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+Email_USE_SSL=True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'raconnectors@gmail.com'
+EMAIL_HOST_PASSWORD = 'billu717'
+EMAIL_PORT = 587
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
